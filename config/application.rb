3da6bac8f7c182be4b8341b2c192d7014578d3c4
Amber <amber@amberwilkie.com>
2017-05-02 09:50:55 +0200

    Personalize for meetingbuddy, add secrets file

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 
-module BlankRailsScaffold
+module MeetingBuddy
   class Application < Rails::Application
     config.generators do |generate|
       generate.helper false
