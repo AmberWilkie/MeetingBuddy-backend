59e349474282e8dad1607a0ccf384a5836869d33
Amber <amber@amberwilkie.com>
2017-05-06 15:38:32 +0200

    Go back to vote:params

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,7 @@ class VotesController < ApplicationController
   before_action :get_meeting_and_points
 
   def create
-    vote = Vote.new(votable: @agenda_point, user: User.first, vote: params[:vote][:vote])
+    vote = Vote.new(votable: @agenda_point, user: User.first, vote: params[:vote])
     if vote.save
       render json: { status: 'success',
                      yes_votes: @agenda_point.votes.where(vote: true).count,
@@ -17,7 +17,7 @@ class VotesController < ApplicationController
 
   def update
     vote = Vote.find(params[:id])
-    vote.vote = params[:vote][:vote]
+    vote.vote = params[:vote]
     if vote.save
       render json: { status: 'success',
                      yes_votes: @agenda_point.votes.where(vote: true).count,
