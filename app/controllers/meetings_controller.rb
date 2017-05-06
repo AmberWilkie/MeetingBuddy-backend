f1f228198519272e3cd9eff5385d5de2ffd5de14
Amber <amber@amberwilkie.com>
2017-05-06 10:51:14 +0200

    Bunch of stuff - add user to meetings, create agenda points controller

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -3,4 +3,11 @@ class MeetingsController < ApplicationController
     @meetings = Meeting.all
     render json: {meetings: @meetings}
   end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+    render json: { meeting: @meeting,
+                   user: @meeting.user
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :meetings , defaults: { format: :json }, only: [:index, :show]
+  resources :meetings , defaults: { format: :json }, only: [:index, :show] do
+    resources :agenda_points , defaults: { format: :json }, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
